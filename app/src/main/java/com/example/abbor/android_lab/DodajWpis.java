073f73a9573ffce5de4668344aaccacfa4c0e715
Aleksandra Borowiec <abborowiec@outlook.com>
2017-06-17 16:06:33 +0200

    skonczone dodawanie wpisu do listy

--- app/src/main/java/com/example/abbor/android_lab/DodajWpis.java
+++ app/src/main/java/com/example/abbor/android_lab/DodajWpis.java
@@ -1,7 +1,10 @@
 package com.example.abbor.android_lab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class DodajWpis extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class DodajWpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dodaj_wpis);
+    }
+
+    public void wyslij (View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String pole = editText.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra("wpis", pole);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
