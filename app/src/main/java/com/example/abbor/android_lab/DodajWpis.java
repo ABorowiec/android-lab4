2ea794cee90e5f53b5ffa820f73ddc33e5e1cd77
Aleksandra Borowiec <abborowiec@outlook.com>
2017-06-17 17:36:50 +0200

    dodanie walidacji

--- app/src/main/java/com/example/abbor/android_lab/DodajWpis.java
+++ app/src/main/java/com/example/abbor/android_lab/DodajWpis.java
@@ -46,16 +46,20 @@ public class DodajWpis extends AppCompatActivity {
         EditText wielkosc = (EditText)findViewById (R.id.editText_wielkosc);
         EditText opis = (EditText) findViewById (R.id.editText_opis);
         Spinner gatunek = (Spinner) findViewById(R.id.spinner_gatunek);
-        Animal zwierze = new Animal(
-                gatunek.getSelectedItem().toString(),
-                kolor.getText().toString(),
-                Float.valueOf(wielkosc.getText().toString()),
-                opis.getText().toString()
-        );
-        zwierze.setId(this.modify_id);
-        Intent intencja = new Intent();
-        intencja.putExtra("nowy", zwierze);
-        setResult(RESULT_OK, intencja);
-        finish();
+        if (! (kolor.getText().toString().equals("") || wielkosc.getText().toString().equals("") || opis.getText().toString().equals(""))) {
+            Animal zwierze = new Animal(
+                    gatunek.getSelectedItem().toString(),
+                    kolor.getText().toString(),
+                    Float.valueOf(wielkosc.getText().toString()),
+                    opis.getText().toString()
+            );
+            zwierze.setId(this.modify_id);
+            Intent intencja = new Intent();
+            intencja.putExtra("nowy", zwierze);
+            setResult(RESULT_OK, intencja);
+            finish();
+        } else {
+            //TODO for later graphical validation
+        }
     }
 }
